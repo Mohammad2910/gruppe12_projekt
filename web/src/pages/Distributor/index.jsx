67e0abc4b29c08bcca9f616e46f610406b8fb3f6
Mohammad Uddin <s184174@student.dtu.dk>
2022-11-06 16:19:58 +0100

    distributor page using simple data for sidegrid

--- web/src/pages/Distributor/index.jsx
+++ web/src/pages/Distributor/index.jsx
@@ -4,15 +4,27 @@ import MapContainer from "../../components/map/map";
 import { distributorStore } from '../../stores/DistributorStore'
 
 import './style.css';
+import {Link} from "react-router-dom";
+import React from "react";
 
 const Distributor = () => {
     return (
         <>
         <div className="distributors">
-            {/*<Grid container spacing={0.5}>*/}
-            <SideGrid data={distributorStore.distributors}/>
+            <div className='wrapper'>
+                <div className='container'>
+                    {distributorStore.distributors.map((data) => {
+                        return (
+                            <div className='item'>
+                                <Link underline='hover' to=''>
+                                    {data.title}
+                                </Link>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
                 <MapContainer/>
-            {/*</Grid>*/}
         </div>
         </>
     )
